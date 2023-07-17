e75afddc142c7ab8bccbbf6e0e587f68ed4506af
FrancisSullivan <francissullivan94@outlook.com>
2023-07-17 11:52:09 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Malin_Space_Science_Systems_Satellite_Data_Processor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
